671f873c2dafc46fe0c8cb638fd55f9acd260fbf
Trent <65882951+Dre-OS@users.noreply.github.com>
2025-07-02 17:54:42 +0800

    refactor: update composePublisher function to improve parameter handling and message publishing logic

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const cors = require('cors');
+// const express = require('express');
+// const cors = require('cors');
 // const port = 3002;
 const amqp = require('amqplib');
-const app = express();
-app.use(cors());
-app.use(express.json());
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
 
 async function connect(uri) {
     try {
@@ -12,7 +12,7 @@ async function connect(uri) {
             uri = 'amqp://guest:guest@localhost:5672';
             console.warn("No RabbitMQ URI provided, using default 'amqp://guest:guest@localhost:5672'");
         }
-        const connection = await amqp.connect(uri || 'amqp://guest:guest@localhost:5672');
+        const connection = await amqp.connect(uri);
         const channel = await connection.createChannel();
         console.log("Connected to RabbitMQ successfully");
         return { connection, channel };
@@ -23,8 +23,7 @@ async function connect(uri) {
 }
 
 
-function composePublisher({connectionUri, exchange, exchangeType, routingKey, queue, options}) {
-  
+function composePublisher({exchange, exchangeType, routingKey, queue, headers, options}) {
   const defaultOptions = {
     durable: true,
     exclusive: false,
@@ -32,16 +31,25 @@ function composePublisher({connectionUri, exchange, exchangeType, routingKey, qu
   }
 
   
-  return async (message) => {
+  return async (channel, message) => {
     try {
-      const {connection, channel} = await connect(connectionUri);
+      // const {connection, channel} = await connect(connectionUri);
 
       {exchange ?
         channel.assertExchange(exchange, exchangeType, options || defaultOptions) :
         channel.assertQueue(queue, options || defaultOptions)
       }
 
-      channel.publish(exchange, routingKey, Buffer.from(message));
+      if (exchangeType === 'fanout') {
+        channel.publish(exchange, '', Buffer.from(message));
+      } else if (exchangeType === 'direct' || exchangeType === 'topic') {
+        channel.publish(exchange, routingKey, Buffer.from(message)) 
+      } else if (exchangeType === 'headers') {
+        channel.publish(exchange, routingKey, Buffer.from(message), { headers });
+      }
+      else {
+        throw new Error(`Unsupported exchange type: ${exchangeType}`);
+      }
       console.log(`Message published to ${exchange} with routing key ${routingKey}`);
     } catch (error) {
       console.error('Error publishing message:', error);
@@ -50,7 +58,12 @@ function composePublisher({connectionUri, exchange, exchangeType, routingKey, qu
   
 }
 
+// app.listen(port, "0.0.0.0", () => {
+//   console.log(`Publisher service is running on port ${port}`);
+// });
+
 module.exports = {
+  connect,
   composePublisher
 }
 
